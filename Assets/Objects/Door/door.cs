dc6512f04ba02bc50b3dce7422c3299bcef653a6
bubbleTJester <jacobmcinnessneddoncshnd@gmail.com>
2025-02-23 21:25:46 +0000

    Changed generation entirely

--- Assets/Objects/Door/door.cs
+++ Assets/Objects/Door/door.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
     [SerializeField] Animator doorAnim;
+    [SerializeField] bool doorEnabled = true;
     public GameObject areaToSpawn;
+    private void Start()
+    {
+        // normally I would just add this in manually for a prefab but for some reason it doesn't seem to carry over when instantiated
+        doorAnim = GetComponentInChildren<Animator>(); 
+    }
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || doorEnabled == true)
         {
             doorAnim.SetTrigger("OpenDoor");
 
             areaToSpawn.SetActive(true);
+            doorEnabled = false;
         }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
     }
 }
